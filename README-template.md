74f8b97a4ab243c99ebda64ed5019229797017db
Samuel Adjei <info.samadjei@gmail.com>
2021-07-18 19:20:47 +0100

    first commit

---
+++ README-template.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JavaScript
+
+### What I learned
+
+I undertook this challenge as a way to refresh my understanding of JavaScript. It was really helpful in getting me back to the basics and i enjoyed the challenge it gave me as an entry-level project to improving my vanilla js skills. 
+
+
+## Author
+
+- Website - [Add your name here](https://www.samadjei.com)
+- Frontend Mentor - [@samadjei](https://www.frontendmentor.io/profile/samadjei)
+- Twitter - [@samkadjei](https://www.twitter.com/samkadjei)
---
+++ js/app.js
@@ -0,0 +1,88 @@
+let tipBtn = document.querySelectorAll('.tip-text');
+let tipPercentage = document.querySelectorAll('.tip-percentage');
+let resetBtn = document.querySelector('.reset');
+let billInput = document.querySelector('.bill-input');
+let peopleInput = document.querySelector('.people-input');
+let customTipInput = document.querySelector('.custom-tip');
+let activeBtn = document.querySelector('.active');
+let errorText = document.querySelector('.error-text');
+
+// Calculate the values of the tips
+function calculate() {
+	const principal = parseFloat(billInput.value);
+	const percent = selectedTip / 100;
+	const people = parseFloat(peopleInput.value);
+	const amount = (principal * percent) / people;
+	const total = principal / people + amount;
+	tipPerson.textContent = '$' + amount.toFixed(2);
+	totalPerson.textContent = '$' + total.toFixed(2);
+	return;
+}
+
+// Error Validation
+peopleInput.addEventListener('input', (e) => {
+	const currentInput = e.target.value;
+	if (currentInput == 0) {
+		peopleInput.classList.add('error');
+		errorText.style.display = 'block';
+	} else {
+		peopleInput.classList.remove('error');
+		errorText.style.display = 'none';
+	}
+
+	calculate();
+});
+
+// Grabbing the value from tip % btn
+tipPercentage.forEach((input) => {
+	input.addEventListener('click', (e) => {
+		selectedTip = parseFloat(e.target.textContent);
+	});
+});
+
+// Grabs the input of the custom tip value
+customTipInput.addEventListener('input', (e) => {
+	const customTip = e.target.value;
+	selectedTip = parseFloat(customTip);
+	calculate();
+});
+
+// Remove the active class when the custom input has been clicked
+customTipInput.addEventListener('click', () => {
+	activeBtn.classList.remove('active');
+});
+
+// Setting the values for the calculated numbers
+let tipPerson = document.querySelector('.tip-person');
+let totalPerson = document.querySelector('.total-person');
+tipPerson.textContent = '$0.00';
+totalPerson.textContent = '$0.00';
+
+function clearContent() {
+	// Clear input values
+	billInput.value = '';
+	peopleInput.value = '';
+	customTipInput.value = '';
+
+	// Clear calculated values
+	tipPerson.textContent = '$0.00';
+	totalPerson.textContent = '$0.00';
+
+	// Clear input validation
+	peopleInput.classList.remove('error');
+	errorText.style.display = 'none';
+}
+
+resetBtn.addEventListener('click', clearContent);
+
+// Lopps through the Select Tips % buttons and add/removes the active class
+tipBtn.forEach((tip) => {
+	tip.addEventListener('click', () => {
+		// removes the original active class from element
+		activeBtn.classList.remove('active');
+		// adds the class on the button that has been clicked
+		tip.classList.add('active');
+		// making activeBtn equal to tipBtn allows you to add and remove the active class
+		activeBtn = tip;
+	});
+});
